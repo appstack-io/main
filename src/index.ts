a4b3dd48e13bb3a46503552b26f82fe12d1dbad7
orgal <or.gal@vee.com>
2023-12-09 15:53:01 +0200

    fix: init

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,1 @@
-import '@appstack-io/config';
-import { MainGrpcModule } from './grpc/main.grpc.module';
-
-(async () => {
-  const { main } = await import('./main');
-  await main({
-    publicMicroservicesModule: MainGrpcModule,
-    ports: {
-      proto: Number(process.env.PROTO_PORT),
-      protoInternal: Number(process.env.PROTO_INTERNAL_PORT),
-      http: Number(process.env.HTTP_PORT),
-      httpInternal: Number(process.env.HTTP_INTERNAL_PORT),
-      workers: Number(process.env.WORKERS_PORT),
-    },
-    otel: true,
-  });
-})();
+export * from './main';
